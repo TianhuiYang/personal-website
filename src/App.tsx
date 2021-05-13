c0586b0419bfab8956a1952a917c1af5545da913
Tianhui Yang <tianhui.yg@gmail.com>
2021-05-12 20:36:13 -0400

    feat: setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import { About } from "./pages/About";
+import { Experience } from "./pages/Experience";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Switch>
+        <Router>
+          <Route exact path="/" component={Experience} />
+          <Route path="/experience" component={Experience} />
+          <Route path="/about" component={About} />
+          {/* <Route path="/contact" component={Contact} /> */}
+        </Router>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
